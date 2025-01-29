3282922b18f9c180cd55685865fc9d50565866bb
Jhean Antunes Lopes <brizadaojhean@gmail.com>
2025-01-29 13:05:20 -0300

    add arithmetic operators examples in index.ts

---
+++ src/operadores/aritmetico/index.ts
@@ -0,0 +1,42 @@
+/*
+  Operadores aritméticos
+  adição +
+  subtração -
+  multiplicação *
+  divisão /
+  módulo %
+  incremento ++
+  descrimento -- 
+ */
+
+// Adição
+const number1 = 1;
+const number2 = 2;
+const adicao = number1 + number2;
+console.log(adicao); // 3
+
+// Subtração
+const subtracao = number1 - number2;
+console.log(subtracao); // 1
+
+// Multiplicação
+const multiplicacao = number1 * number2;
+console.log(multiplicacao); // 2s
+
+// Divisão
+const divisao = number1 / number2;
+console.log(divisao); // 0.5
+
+// Módulo
+const modulo = number1 % number2;
+console.log(modulo); // 1
+
+// Incremento
+let incremento = 1;
+incremento++;
+console.log(incremento); // 2
+
+// Decremento
+let decremento = 1;
+decremento--;
+console.log(decremento); // 0
