dc9a6a22da5febbcb2601352317f5bc030bec9fb
Jhean Antunes Lopes <brizadaojhean@gmail.com>
2025-01-29 12:36:49 -0300

    add array data examples in index.ts

---
+++ src/array/index.ts
@@ -0,0 +1,11 @@
+const pokemons: string[] = ["Pikachu", "Charmander", "Bulbasaur", "Squirtle"];
+
+const randomNum: number[] = [1, 2, 3, 4, 5, 6, 7, 8, 9, 10];
+
+const users = [
+  { name: "John", age: 25 },
+  { name: "Jane", age: 24 },
+  { name: "Doe", age: 26 },
+];
+
+export { pokemons, randomNum, users };
