d6423cfea2e4a8801a364f36a0ab93a4f6b469e8
Jhean Antunes Lopes <brizadaojhean@gmail.com>
2025-01-30 11:59:56 -0300

    add if-else statement examples in src/if-else/index.ts

---
+++ src/if-else/index.ts
@@ -0,0 +1,9 @@
+// if, else if, else
+const age = 18;
+if (age < 18) {
+  console.log("You are a minor.");
+} else if (age >= 18 && age < 60) {
+  console.log("You are an adult.");
+} else {
+  console.log("You are a senior citizen.");
+}
