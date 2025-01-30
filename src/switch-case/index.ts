e2e3384994481a2f2483b18029dcfb0989459f9e
Jhean Antunes Lopes <brizadaojhean@gmail.com>
2025-01-30 12:15:53 -0300

    add switch-case statement examples in src/switch-case/index.ts

---
+++ src/switch-case/index.ts
@@ -0,0 +1,13 @@
+// switch-case
+const idade: number = 18;
+switch (idade) {
+  case 18:
+    console.log("Você tem 18 anos");
+    break;
+  case 19:
+    console.log("Você tem 19 anos");
+    break;
+  default:
+    console.log("Você tem outra idade");
+    break;
+}
